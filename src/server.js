a8612fab5db2030169d58fc49f51335faff20161
Makemyappdotco <makemyapp.co@gmail.com>
2026-07-23 19:40:24 +0530

    Bust cache for perfected paid profile header

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ function sendLandingWithPatches(res) {
   const paidScript = '<script src="/paid-live-flow.js?v=paid-live-ui-2"></script>';
   const polishScript = '<script src="/landing-live-polish.js?v=landing-polish-2"></script>';
   const modalFixScript = '<script src="/paid-modal-scroll-photo.js?v=paid-modal-profile-2"></script>';
-  const profileRepairScript = '<script src="/paid-profile-repair.js?v=paid-profile-repair-1"></script>';
+  const profileRepairScript = '<script src="/paid-profile-repair.js?v=paid-profile-repair-2"></script>';
   const freeDownloadFixScript = '<script src="/free-download-top-fix.js?v=free-download-position-2"></script>';
 
   html = html.replace(/<script src="\/paid-test-flow\.js[^>]*><\/script>/g, '');

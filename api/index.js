2cf31c7e906cb4d624a056ec3fb8689305c1b61e
Makemyappdotco <makemyapp.co@gmail.com>
2026-08-27 00:53:05 +0530

    Expose preview-only report layout QA route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 require('../src/services/locationSearchUpgrade')();
 const app = require('../src/server.js');
-module.exports = app;
+const pdfLayoutV2Preview = require('./pdf-layout-v2-preview.js');
+
+module.exports = async function handler(req, res) {
+  const path = String(req.url || '').split('?')[0];
+  if (req.method === 'GET' && path === '/api/pdf-layout-v2-preview') {
+    return pdfLayoutV2Preview(req, res);
+  }
+  return app(req, res);
+};

de1424ea636ca5bbe73f81ef6705d96647de294e
Makemyappdotco <makemyapp.co@gmail.com>
2026-08-27 18:19:55 +0530

    Expose V4 PDF preview route on preview branch

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 require('../src/services/locationSearchUpgrade')();
 const app = require('../src/server.js');
-module.exports = app;
+const pdfV4Preview = require('./pdf-layout-v4-preview');
+
+module.exports = async function handler(req, res) {
+  const requestPath = String(req.url || '').split('?')[0];
+  if (req.method === 'GET' && requestPath === '/api/pdf-layout-v4-preview') {
+    return pdfV4Preview(req, res);
+  }
+  return app(req, res);
+};

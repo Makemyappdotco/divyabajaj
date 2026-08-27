d4bdc9bff690096bb07d049e64e2fd67d9a30d86
Makemyappdotco <makemyapp.co@gmail.com>
2026-08-27 16:04:19 +0530

    Route paid reports through branded continuous renderer

--- src/services/pdf.js
+++ src/services/pdf.js
@@ -1,12 +1,12 @@
 const legacyPdf = require('./pdfLegacy');
-const { generateStructuredPaidPdf } = require('./pdfStructuredV2');
+const { generateStructuredPaidPdfV3 } = require('./pdfStructuredV3');
 
 function isPaidReport(report = {}) {
   return String(report.type || '').toLowerCase().includes('paid');
 }
 
 async function generateReportPdf(args = {}) {
-  if (isPaidReport(args.report)) return generateStructuredPaidPdf(args);
+  if (isPaidReport(args.report)) return generateStructuredPaidPdfV3(args);
   return legacyPdf.generateReportPdf(args);
 }
 
